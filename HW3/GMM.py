44b622923091d0e556432311f77d8c558de312b2
CK1201 <ckwu1201@163.com>
2021-10-19 20:20:29 +0800

    Complete HW3

--- HW3/GMM.py
+++ HW3/GMM.py
@@ -11,7 +11,7 @@ from scipy.stats import multivariate_normal
 plt.style.use('seaborn')
 
 class GMM(object):
-    def __init__(self, n_clusters, max_iter=50):
+    def __init__(self, n_clusters, max_iter = 50):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
     
@@ -20,7 +20,7 @@ class GMM(object):
     
 
     # 更新pi
- 
+
         
     # 更新Mu
 
@@ -33,14 +33,69 @@ class GMM(object):
     def fit(self, data):
         # 作业3
         # 屏蔽开始
+        self.pi = np.ones(self.n_clusters) / self.n_clusters
+        self.cov = np.array([np.diag(np.ones(data.shape[1])) for i in range(self.n_clusters)])
+        # print(self.var)
+        row_rand_array = np.arange(data.shape[0])
+        np.random.shuffle(row_rand_array)
+        self.mu = data[row_rand_array[0: self.n_clusters]]
+        N_nk = np.zeros((self.n_clusters, data.shape[0]))
+        iter = 0
+        # print(self.mu)
+        while iter < self.max_iter:
+            iter += 1
 
+            for k in range(self.n_clusters):
+                N_nk[k] = multivariate_normal.pdf(data, mean = self.mu[k], cov = self.cov[k])
+            N_nk = np.dot(np.diag(self.pi.ravel()), N_nk)
+            N_nk /= np.sum(N_nk, axis=0)
+            N_k = np.sum(N_nk, axis = 1)
+            self.mu = np.asarray([np.dot(N_nk[k], data) / N_k[k] for k in range(self.n_clusters)])
+            self.cov = np.asarray([np.dot((data - self.mu[k]).T, np.dot(np.diag(N_nk[k].ravel()), data - self.mu[k]))/N_k[k] for k in range(self.n_clusters)])
+            self.pi = N_k / data.shape[0]
 
+            # for i in range(data.shape[0]):
+            #     for j in range(self.n_clusters):
+            #         # print(self.var[j])
+            #         # print((data[i] - self.mu[j]).reshape(data.shape[1], 1))
+            #         # print(np.linalg.inv(self.var[j]))
+            #         # print(np.dot((data[i] - self.mu[j]), np.linalg.inv(self.var[j])).dot((data[i] - self.mu[j]).reshape(data.shape[1], 1)))
+            #         # print(np.dot((data[i] - self.mu[j]), np.linalg.inv(self.var[j])).dot(data[i] - self.mu[j]))
+            #         N_nk[i, j] = 1 / (2 * math.pi) ** (data.shape[1] / 2) / np.linalg.det(self.var[j]) ** (1 / 2) * exp(-1 / 2 * np.dot((data[i] - self.mu[j]), np.linalg.inv(self.var[j])).dot((data[i] - self.mu[j]).reshape(data.shape[1], 1)))
+            #         sum_N_nk[i] += N_nk[i, j]
+            #         N_k[j] += N_nk[i, j]
+            #     for j in range(self.n_clusters):
+            #         gamma_nk[i, j] = self.pi[j] * N_nk[i, j] / sum_N_nk[i]
+
+            # for k in range(self.n_clusters):
+            #     # print(gamma_nk[:, k])
+            #     self.mu[k] = 0
+            #     self.var[k] = 0
+            #     for i in range(data.shape[0]):
+            #         self.mu[k] += 1 / N_k[k] * data[i] * gamma_nk[i, k]
+            #         self.var[k] += 1 / N_k[k] * gamma_nk[i, k] * np.dot((data[i] - self.mu[k]).reshape(data.shape[1], 1), (data[i] - self.mu[k]).reshape(1, data.shape[1]))
+            #         # print((data[i] - self.mu[k]).reshape(data.shape[1], 1))
+            #         # print((data[i] - self.mu[k]).reshape(1, data.shape[1]))
+            #         # print(np.dot((data[i] - self.mu[k]).reshape(data.shape[1], 1), (data[i] - self.mu[k]).reshape(1, data.shape[1])))
+            #     self.pi[k] = N_k[k] / data.shape[0]
+            # print(self.mu)
+            # print(self.var)
+            # print(self.pi)
         # 屏蔽结束
     
     def predict(self, data):
         # 屏蔽开始
-
+        N_nk = np.zeros((self.n_clusters, data.shape[0]))
+        for k in range(self.n_clusters):
+            N_nk[k] = multivariate_normal.pdf(data, mean=self.mu[k], cov=self.cov[k])
+        N_nk = np.dot(np.diag(self.pi.ravel()), N_nk)
+        N_nk /= np.sum(N_nk, axis=0)
+        result_set = np.argmax(N_nk, axis=0)
+        return result_set
         # 屏蔽结束
+
+    def get_mu(self):
+        return self.mu
 
 # 生成仿真数据
 def generate_X(true_Mu, true_Var):
@@ -58,10 +113,10 @@ def generate_X(true_Mu, true_Var):
     # 显示数据
     plt.figure(figsize=(10, 8))
     plt.axis([-10, 15, -5, 15])
-    plt.scatter(X1[:, 0], X1[:, 1], s=5)
-    plt.scatter(X2[:, 0], X2[:, 1], s=5)
-    plt.scatter(X3[:, 0], X3[:, 1], s=5)
-    plt.show()
+    plt.scatter(X1[:, 0], X1[:, 1], s = 5)
+    plt.scatter(X2[:, 0], X2[:, 1], s = 5)
+    plt.scatter(X3[:, 0], X3[:, 1], s = 5)
+    # plt.show()
     return X
 
 if __name__ == '__main__':
@@ -69,12 +124,28 @@ if __name__ == '__main__':
     true_Mu = [[0.5, 0.5], [5.5, 2.5], [1, 7]]
     true_Var = [[1, 3], [2, 2], [6, 2]]
     X = generate_X(true_Mu, true_Var)
-
-    gmm = GMM(n_clusters=3)
+    # print(X)
+    gmm = GMM(n_clusters = 3)
     gmm.fit(X)
-    cat = gmm.predict(X)
-    print(cat)
-    # 初始化
+    # cat = gmm.predict(X)
+    # print(cat)
+    category = gmm.predict(X)
+
+    # visualize:
+    color = ['red', 'blue', 'green', 'cyan', 'magenta']
+    labels = [f'Cluster{k:02d}' for k in range(3)]
+
+    for k in range(3):
+        plt.scatter(X[category == k][:, 0], X[category == k][:, 1], c=color[k], label=labels[k])
+
+    mu = gmm.get_mu()
+    plt.scatter(mu[:, 0], mu[:, 1], s=300, c='grey', marker='P', label='Centroids')
+
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.title('GMM Testcase')
+    plt.show()
 
     
 
--- HW3/KMeans.py
+++ HW3/KMeans.py
@@ -1,6 +1,7 @@
 # 文件功能： 实现 K-Means 算法
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class K_Means(object):
     # k是分组数；tolerance‘中心点误差’；max_iter是迭代次数
@@ -9,25 +10,77 @@ class K_Means(object):
         self.tolerance_ = tolerance
         self.max_iter_ = max_iter
 
+
     def fit(self, data):
         # 作业1
         # 屏蔽开始
+        iter = 0
+        row_rand_array = np.arange(data.shape[0])
+        np.random.shuffle(row_rand_array)
+        self.clusters_center = data[row_rand_array[0: self.k_]]
+        old_center = self.clusters_center.copy()
+        # print(self.clusters_center)
+        label = np.zeros(data.shape[0])
+        while(iter <= self.max_iter_):
+            iter += 1
+            for i in range(data.shape[0]):
+                dist = np.zeros(self.k_)
+                for k in range(self.k_):
+                    dist[k] = np.linalg.norm(self.clusters_center[k] - data[i])
+                # print(np.where(dist == np.min(dist))[0])
+                label[i] = np.where(dist == np.min(dist))[0]
+            # print(label)
 
+            dist_center = np.zeros(self.k_)
+            for k in range(self.k_):
+                self.clusters_center[k] = np.mean(data[label == k])
+                dist_center[k] = np.linalg.norm(self.clusters_center[k] - old_center[k])
+            # print(old_center)
+            # print(self.clusters_center)
+            # print(dist_center)
+            if dist_center.all() < self.tolerance_:
+                break
+            else:
+                old_center = self.clusters_center.copy()
+        # print("origin label: ", label)
         # 屏蔽结束
 
     def predict(self, p_datas):
-        result = []
+        result = np.zeros(p_datas.shape[0])
         # 作业2
         # 屏蔽开始
-
+        dist = np.zeros(self.k_)
+        for i in range(p_datas.shape[0]):
+            for k in range(self.k_):
+                dist[k] = np.linalg.norm(self.clusters_center[k] - p_datas[i])
+            result[i] = np.where(dist == np.min(dist))[0]
         # 屏蔽结束
         return result
 
+    def get_centroids(self):
+        return self.clusters_center
+
 if __name__ == '__main__':
     x = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
-    k_means = K_Means(n_clusters=2)
+    # print(x)
+    n_clusters = 2
+    k_means = K_Means(n_clusters = n_clusters)
     k_means.fit(x)
 
-    cat = k_means.predict(x)
-    print(cat)
+    category = k_means.predict(x)
+
+    # visualize:
+    color = ['red', 'blue', 'green', 'cyan', 'magenta']
+    labels = [f'Cluster{k:02d}' for k in range(n_clusters)]
+    for k in range(n_clusters):
+        plt.scatter(x[category == k][:, 0], x[category == k][:, 1], c=color[k], label=labels[k])
+
+    centroids = k_means.get_centroids()
+    plt.scatter(centroids[:, 0], centroids[:, 1], s=300, c='grey', marker='P', label='Centroids')
+
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.title('KMeans Testcase')
+    plt.show()
 
